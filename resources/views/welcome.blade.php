477ea01b04ba3f3574c5b7bad61ca76e75fb2e60
unknown <nizar.talbany@emsi-edu.ma>
2025-07-03 13:22:10 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card shadow">
+                <div class="card-body text-center">
+                    <h1 class="text-primary mb-4">Bienvenue sur Medical MarketPlace</h1>
+                    <p class="lead">Votre plateforme pour le matériel médical.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
